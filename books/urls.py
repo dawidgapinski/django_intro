39d4eff6d96e939bf01858409d6d3e91cc7fb71b
Dawid Gapiński <dawid117v@wp.pl>
2022-04-24 11:43:16 +0200

    Adde category_list.html

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
-    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView
+    check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, CategoryListTemplateView
 
 urlpatterns = [
     path('uuids-a', get_uuids_a),
@@ -12,4 +12,5 @@ urlpatterns = [
     path('get-headers', get_headers, name="get_headers"),
     path('raise-error', raise_error_for_fun, name="raise_error"),
     path('author-list', AuthorListBaseView.as_view(), name="author_list"),
+    path('category-list', CategoryListTemplateView.as_view(), name="category_list"),
 ]
