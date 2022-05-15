689660589ae35f4355bf922f3cc542b38e7a83be
Dawid Gapiński <dawid117v@wp.pl>
2022-05-15 14:03:14 +0200

    Context

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from users.views import SignUpView, UserLoginView, UserListView
+from users.views import SignUpView, UserLoginView, UserListView, list_of_users
 
 urlpatterns = [
     path('sign-up/', SignUpView.as_view(), name='sign_up'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('list/', UserListView.as_view(), name='user_list'),
+    path('david-list/', list_of_users.as_view(), name='david_list'),
 
 
 ]
